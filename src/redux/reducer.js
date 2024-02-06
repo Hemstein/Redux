c10effaf800ce81748a364e4068c2709029b2cf3
Hemstein <haythem_benjemaa@hotmail.de>
2024-02-06 20:44:23 +0100

    first commit

---
+++ src/redux/reducer.js
@@ -0,0 +1,23 @@
+
+
+const initialState={Todo:[
+    {id:0, description:'Ahla', isdone:true},
+    {id:1, description:'wa', isdone:false},
+    {id:2, description:'sahla', isdone:true}
+]
+}
+export const Todoreducer=(state=initialState,action)=>{
+switch (action.type) {
+    case add:
+        return
+    case remove:
+        return
+    case edit:
+        return
+        
+        
+
+    default:
+        
+}
+}
